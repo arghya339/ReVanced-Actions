f6a7fa410cb1deb2297902bdf00e4837963b1006
Arghya <arghya339@chromium.org>
2024-11-01 14:14:20 +0530

    fix

--- src/build/Arghya-Revanced-Extended-Android-6-7.sh
+++ src/build/Arghya-Revanced-Extended-Android-6-7.sh
@@ -13,18 +13,18 @@ get_apk "com.google.android.youtube" "youtube" "youtube" "google-inc/youtube/you
 split_editor "youtube" "youtube"
 patch "youtube" "revanced-extended-android-6-7-arghya"
 # Patch Youtube Arm64-v8a
-get_patches_key "youtube-revanced-extended-6-7-arghya"
+get_patches_key "youtube-rve-6-7-arghya"
 split_editor "youtube" "youtube-arm64-v8a" "exclude" "split_config.armeabi_v7a split_config.x86 split_config.x86_64"
 patch "youtube-arm64-v8a" "revanced-extended-android-6-7-arghya"
 # Patch Youtube Armeabi-v7a
-get_patches_key "youtube-revanced-extended-6-7-arghya"
+get_patches_key "youtube-rve-6-7-arghya"
 split_editor "youtube" "youtube-armeabi-v7a" "exclude" "split_config.arm64_v8a split_config.x86 split_config.x86_64"
 patch "youtube-armeabi-v7a" "revanced-extended-android-6-7-arghya"
 # Patch Youtube x86
-get_patches_key "youtube-revanced-extended-6-7-arghya"
+get_patches_key "youtube-rve-6-7-arghya"
 split_editor "youtube" "youtube-x86" "exclude" "split_config.arm64_v8a split_config.armeabi_v7a split_config.x86_64"
 patch "youtube-x86" "revanced-extended-android-6-7-arghya"
 # Patch Youtube x86_64
-get_patches_key "youtube-revanced-extended-6-7-arghya"
+get_patches_key "youtube-rve-6-7-arghya"
 split_editor "youtube" "youtube-x86_64" "exclude" "split_config.arm64_v8a split_config.armeabi_v7a split_config.x86"
 patch "youtube-x86_64" "revanced-extended-android-6-7-arghya"
